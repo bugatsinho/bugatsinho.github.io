aaa1896c3a49a1fe597cade5c36ddd3cef673be0
bugatsinho <bugatsinho@protonmail.com>
2018-12-02 00:53:43 -0500

    add domain selection

--- plugin.video.releaseBB/default.py
+++ plugin.video.releaseBB/default.py
@@ -8,7 +8,6 @@ import urlparse
 import re
 import sys
 import os
-import resolveurl
 import random
 import json
 import threading
@@ -19,18 +18,18 @@ from resources.lib.modules import dom_parser as dom
 from t0mm0.common.addon import Addon
 from t0mm0.common.net import Net
 
-
 try:
     from sqlite3 import dbapi2 as sqlite
+
     print "Loading sqlite3 as DB engine"
 except BaseException:
     from pysqlite2 import dbapi2 as sqlite
+
     print "Loading pysqlite2 as DB engine"
 
 addon_id = 'plugin.video.releaseBB'
 plugin = xbmcaddon.Addon(id=addon_id)
 DB = os.path.join(xbmc.translatePath("special://database"), 'cache.db')
-BASE_URL = 'http://rlsbb.ru'
 net = Net()
 addon = Addon('plugin.video.releaseBB', sys.argv)
 
@@ -59,26 +58,29 @@ NAME = ADDON.getAddonInfo('name')
 version = ADDON.getAddonInfo('version')
 IconPath = control.addonPath + "/resources/icons/"
 
+base = control.setting('domain')
+BASE_URL = 'http://%s' % base.lower()
 
-def MainMenu():    #homescreen
+
+def MainMenu():  # homescreen
     addon.add_directory({'mode': 'Categories', 'section': 'movies'},
-                        {'title':  '[COLOR orange][B]Release BB [COLOR blue]Movies [/COLOR][/B]'},
+                        {'title': '[COLOR orange][B]Release BB [COLOR blue]Movies [/COLOR][/B]'},
                         img=IconPath + 'movie.png', fanart=FANART)
     addon.add_directory({'mode': 'Categories', 'section': 'tv-shows'},
-                        {'title':  '[COLOR orange][B]Release BB [COLOR blue]Tv Shows [/COLOR][/B]'},
+                        {'title': '[COLOR orange][B]Release BB [COLOR blue]Tv Shows [/COLOR][/B]'},
                         img=IconPath + 'tv.png', fanart=FANART)
     addon.add_directory({'mode': 'search_bb'},
-                        {'title':  '[COLOR orange][B]Release BB [COLOR yellow]Search[/COLOR][/B]'},
+                        {'title': '[COLOR orange][B]Release BB [COLOR yellow]Search[/COLOR][/B]'},
                         img=IconPath + 'search.png', fanart=FANART)
     addon.add_directory({'mode': 'settings'}, {'title': '[COLOR cyan][B]Settings-Tools[/B][/COLOR]'},
                         img=ICON, fanart=FANART, is_folder=False)
-    #addon.add_directory({'mode': 'RealDebrid'},  {'title':  '[COLOR gold][B]Real Debrid Auth[/B][/COLOR]'},
+    # addon.add_directory({'mode': 'RealDebrid'},  {'title':  '[COLOR gold][B]Real Debrid Auth[/B][/COLOR]'},
     #                    img=IconPath + 'rd.png', fanart=FANART, is_folder=False)
-    #addon.add_directory({'mode': 'ResolverSettings'}, {'title':  '[COLOR cyan][B]Resolver Settings[/B][/COLOR]'},
+    # addon.add_directory({'mode': 'ResolverSettings'}, {'title':  '[COLOR cyan][B]Resolver Settings[/B][/COLOR]'},
     #                    img=IconPath + 'resolver.png', fanart=FANART, is_folder=False)
-    #addon.add_directory({'mode': 'ClearCache'}, {'title': '[COLOR red][B]Clear Cache[/B][/COLOR]'},
+    # addon.add_directory({'mode': 'ClearCache'}, {'title': '[COLOR red][B]Clear Cache[/B][/COLOR]'},
     #                    img=ICON, fanart=FANART, is_folder=False)
-    addon.add_directory({'mode': 'help'}, {'title':  '[COLOR gold][B][I]https://bugatsinho.github.io[/B][/I][/COLOR]'},
+    addon.add_directory({'mode': 'help'}, {'title': '[COLOR gold][B][I]https://bugatsinho.github.io[/B][/I][/COLOR]'},
                         img='https://bugatsinho.github.io/images/bug.png', fanart=FANART, is_folder=False)
     addon.add_directory({'mode': 'forceupdate'},
                         {'title': '[COLOR gold][B]Version:' + ' [COLOR lime]%s[/COLOR][/B]' % version},
@@ -105,7 +107,7 @@ def Categories(section):  # categories
     xbmcplugin.endOfDirectory(int(sys.argv[1]))
 
 
-def GetTitles(section, url, startPage= '1', numOfPages= '1'): # Get Movie Titles
+def GetTitles(section, url, startPage='1', numOfPages='1'):  # Get Movie Titles
     try:
         if int(startPage) > 1:
             pageUrl = urlparse.urljoin(url, 'page/%d' % int(startPage))
@@ -123,31 +125,32 @@ def GetTitles(section, url, startPage= '1', numOfPages= '1'): # Get Movie Titles
                       client.parseDOM(i, 'a')[0],
                       client.parseDOM(i, 'img', ret='src')[1],
                       client.parseDOM(i, 'div', attrs={'class': 'postContent'})[0]) for i in match if i]
-            #match = re.compile('postHeader.+?href="(.+?)".+?>(.+?)<.+?src=.+? src="(.+?).+?(Plot:.+?)</p>"', re.DOTALL).findall(html)
+            # match = re.compile('postHeader.+?href="(.+?)".+?>(.+?)<.+?src=.+? src="(.+?).+?(Plot:.+?)</p>"', re.DOTALL).findall(html)
             for movieUrl, name, img, desc in match:
                 desc = Sinopsis(desc)
                 img += '|User-Agent=%s&Referer=%s' % (urllib.quote(client.agent()), movieUrl)
                 addon.add_directory({'mode': 'GetLinks', 'section': section, 'url': movieUrl, 'img': img, 'plot': desc},
-                                    {'title':  name, 'plot': desc},
+                                    {'title': name, 'plot': desc},
                                     [('Release BB Settings',
                                       'RunPlugin(plugin://plugin.video.releaseBB/?mode=settings)',),
                                      ('Clear Cache', 'RunPlugin(plugin://plugin.video.releaseBB/?mode=ClearCache)',)],
                                     img=img, fanart=FANART)
             if 'Older Entries' not in html:
-                    break
+                break
         # keep iterating until the last page is reached
         if 'Older Entries' in html:
-                addon.add_directory({'mode': 'GetTitles', 'url': url, 'startPage': str(end), 'numOfPages': numOfPages},
-                                    {'title': '[COLOR orange][B][I]Older Entries...[/B][/I][/COLOR]'},
-                                    img=IconPath + 'nextpage.png', fanart=FANART)
+            addon.add_directory({'mode': 'GetTitles', 'url': url, 'startPage': str(end), 'numOfPages': numOfPages},
+                                {'title': '[COLOR orange][B][I]Older Entries...[/B][/I][/COLOR]'},
+                                img=IconPath + 'nextpage.png', fanart=FANART)
     except BaseException:
         control.infoDialog(
             '[COLOR red][B]Ooops![/B][/COLOR]\n[COLOR lime][B]Something wrong!![/B][/COLOR]', NAME, ICON, 3000)
         xbmcplugin.endOfDirectory(int(sys.argv[1]))
 
 
-def GetLinks(section, url, img, plot): # Get Links
+def GetLinks(section, url, img, plot):  # Get Links
     try:
+        import resolveurl
         html = response_html(url, '3')
         listitem = GetMediaInfo(html)
         name = '%s (%s)' % (listitem[0], listitem[1])
@@ -163,7 +166,7 @@ def GetLinks(section, url, img, plot): # Get Links
                 if 'Unknown' in host:
                     continue
                 # ignore .rar files
-                if any(x in url.lower() for x in ['.rar.', '.zip.', '.iso.'])\
+                if any(x in url.lower() for x in ['.rar.', '.zip.', '.iso.']) \
                         or any(url.lower().endswith(x) for x in ['.rar', '.zip', '.iso']):
                     continue
                 if any(x in url.lower() for x in ['sample', 'zippyshare']):
@@ -178,7 +181,7 @@ def GetLinks(section, url, img, plot): # Get Links
                     title = title.replace('.rar', '[COLOR red][B][I]RAR no streaming[/B][/I][/COLOR]')
                     title = title.replace('rar', '[COLOR red][B][I]RAR no streaming[/B][/I][/COLOR]')
                     title = title.replace('www.', '')
-                    title = title.replace ('-', ' ')
+                    title = title.replace('-', ' ')
                     title = title.replace('_', ' ')
                     title = title.replace('.', ' ')
                     title = title.replace('480p', '[COLOR coral][B][I]480p[/B][/I][/COLOR]')
@@ -260,19 +263,19 @@ def link_tester(item):
             else:
                 if 'nitroflare' in host:
                     r = client.parseDOM(r, 'legend')[0]
-                    #addon.log('@#@RESULT:%s' % r)
+                    # addon.log('@#@RESULT:%s' % r)
                 elif 'rapidgator' in host:
                     r = re.findall('''(File size:.+?)MD5''', r, re.I | re.DOTALL)[0]
-                    #addon.log('@#@RESULT:%s' % r)
+                    # addon.log('@#@RESULT:%s' % r)
                 elif 'turbobit' in host:
                     r = client.parseDOM(r, 'title')[0]
                     r = r.replace('Мб', 'MB')
-                    #addon.log('@#@RESULT:%s' % r)
+                    # addon.log('@#@RESULT:%s' % r)
                 elif any(i in host.lower() for i in ['rockfile', 'clicknupload']):
                     r = 'N/A'
                 elif 'openload' in host or 'oload' in host:
                     r = re.findall('''(File size:.+?)</div>''', r, re.I | re.DOTALL)[0]
-                    #addon.log('@#@RESULT:%s' % r)
+                    # addon.log('@#@RESULT:%s' % r)
                 else:
                     r = r
                 try:
@@ -290,13 +293,13 @@ def link_tester(item):
                     {'title': title, 'plot': plot}, img=img, fanart=FANART, is_folder=False)
 
     except BaseException:
-            addon.log('URL ERROR: [%s]: URL: %s ' % (host.upper(), link))
-        
+        addon.log('URL ERROR: [%s]: URL: %s ' % (host.upper(), link))
+
 
 def PlayVideo(url, title, img, plot):
     try:
+        import resolveurl
         stream_url = resolveurl.resolve(url)
-        addon.log('URL-RESOLVED: %s ' % stream_url)
         liz = xbmcgui.ListItem(title, iconImage="DefaultVideo.png", thumbnailImage=img)
         liz.setInfo(type="Video", infoLabels={"Title": title, "Plot": plot})
         liz.setProperty("IsPlayable", "true")
@@ -318,7 +321,7 @@ def get_size(text):
         return size
     except BaseException:
         return 'N/A'
-    
+
 
 def GetDomain(url):
     elements = urlparse.urlparse(url)
@@ -334,7 +337,7 @@ def GetDomain(url):
 
 def GetMediaInfo(html):
     try:
-        #<h1 class="postTitle" rel="bookmark">American Dresser 2018 BRRip XviD AC3-RBG</h1>
+        # <h1 class="postTitle" rel="bookmark">American Dresser 2018 BRRip XviD AC3-RBG</h1>
         match = client.parseDOM(html, 'h1', attrs={'class': 'postTitle'})[0]
         match = re.findall('(.+?)\s+(\d{4}|S\d+E\d+)', match)[0]
         return match
@@ -418,11 +421,11 @@ class Thread(threading.Thread):
         self._target(*self._args)
 
 
-if mode == 'main': 
+if mode == 'main':
     MainMenu()
 elif mode == 'Categories':
     Categories(section)
-elif mode == 'GetTitles': 
+elif mode == 'GetTitles':
     GetTitles(section, url, startPage, numOfPages)
 elif mode == 'GetLinks':
     GetLinks(section, url, img, plot)
@@ -433,8 +436,9 @@ elif mode == 'PlayVideo':
 elif mode == 'settings':
     control.openSettings()
 elif mode == 'ResolverSettings':
+    import resolveurl
     resolveurl.display_settings()
-#elif mode == 'RealDebrid':
+# elif mode == 'RealDebrid':
 #    xbmc.executebuiltin('XBMC.RunPlugin(plugin://script.module.resolveurl/?mode=auth_rd)')
 elif mode == 'ClearCache':
     cache.delete(control.cacheFile, False)
