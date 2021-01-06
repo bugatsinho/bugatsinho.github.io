c589826fff8a0e6bdb93cd4be5b9dd3e1a8114b4
bugatsinho <bugatsinho@protonmail.com>
2021-01-06 15:13:09 -0500

    Update releaseBB to 2.0.3
    [B][COLOR lime]2.0.3[/B][/COLOR] [COLOR gold](01-01-2021)[/COLOR]
    - corrections in service.py

--- plugin.video.releaseBB/service.py
+++ plugin.video.releaseBB/service.py
@@ -15,11 +15,14 @@ if xbmcvfs.exists(_settingsFile):
         line = re.findall(r'(<setting id="domain".+?/>)', data)[0]
         line2 = re.findall(r'(<setting id="eztv\.domain".+?/>)', data)[0]
 
-        eztv_status = client.request('https://eztvstatus.com')
-        domains = client.parseDOM(eztv_status, 'a', ret='href', attrs={'class': 'domainLink'})
-        domains = [i.split('//')[1].encode('utf-8').upper() for i in domains if domains]
-
-        set_line = '<setting id="eztv.domain" label="EZTV DOMAIN" type="select" values="{}|{}|{}|{}" default="{}"/>'
+        try:
+            eztv_status = client.request('https://eztvstatus.com')
+            domains = client.parseDOM(eztv_status, 'a', ret='href', attrs={'class': 'domainLink'})
+            domains = [i.split('//')[1].encode('utf-8').upper() for i in domains if domains]
+        except IndexError:
+            domains = ['eztv.re', 'eztv.ag', 'eztv.it', 'eztv.ch']
+            domains = [i.encode('utf-8').upper() for i in domains if domains]
+        set_line = '<setting id="eztv.domain" label="EZTV DOMAIN" type="select" values="{0}|{1}|{2}|{3}" default="{0}"/>'
         fline = set_line.format(domains[0], domains[1], domains[2], domains[3], domains[0])
 
         paste = cache.get(client.request, 12, 'https://pastebin.com/raw/upztzeGt')
