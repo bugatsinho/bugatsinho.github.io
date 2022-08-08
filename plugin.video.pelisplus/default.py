9ac9857ed9c251f8e82e64cd366ca0061fcbf497
crowslayer <31776660+crowslayer@users.noreply.github.com>
2022-08-08 15:34:10 -0500

    Update default.py

--- plugin.video.pelisplus/default.py
+++ plugin.video.pelisplus/default.py
@@ -12,7 +12,7 @@ from resources.lib.modules import control
 reload(sys)
 sys.setdefaultencoding('utf8')
 
-BASEURL = 'https://pelisplushd.tv/'
+BASEURL = 'https://www.pelisplus.lat/'
 
 ADDON       = xbmcaddon.Addon()
 ADDON_DATA  = ADDON.getAddonInfo('profile')
