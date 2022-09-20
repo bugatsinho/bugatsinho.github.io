a4e1b8948380a6c05474e6e3dc2d4b14a9d574c4
crowslayer <31776660+crowslayer@users.noreply.github.com>
2022-09-20 15:17:34 -0500

    Update default.py
    Update url

--- plugin.video.pelisplus/default.py
+++ plugin.video.pelisplus/default.py
@@ -12,7 +12,7 @@ from resources.lib.modules import control
 reload(sys)
 sys.setdefaultencoding('utf8')
 
-BASEURL = 'https://ww2.pelisplushd.nu'
+BASEURL = 'https://pelisplushd.is/'
 
 ADDON       = xbmcaddon.Addon()
 ADDON_DATA  = ADDON.getAddonInfo('profile')
