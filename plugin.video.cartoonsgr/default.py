3d3bf217fbdfce64df32e654a2515c332877165d
bugatsinho <bugatsinho@protonmail.com>
2023-09-18 13:25:01 -0500

    update CartoonsGR to 1.0.47
    fix encoding issues on kodi18

--- plugin.video.cartoonsgr/default.py
+++ plugin.video.cartoonsgr/default.py
@@ -107,7 +107,7 @@ def Main_addDir():
 def gamatokids():
     addDir('[B][COLOR yellow]' + Lang(32004) + '[/COLOR][/B]', GAMATO + 'paidika/', 4, ART + 'dub.jpg', FANART, '')
     addDir('[B][COLOR yellow]' + Lang(32010) + '[/COLOR][/B]', GAMATO + 'animation/', 4, ART + 'genre.jpg', FANART, '')
-    addDir('[B][COLOR yellow]Family[/COLOR][/B]', GAMATO + six.ensure_str('χριστουγεννιάτικες'), 4, ART + 'top.png', FANART, '')
+    addDir('[B][COLOR yellow]Family[/COLOR][/B]', GAMATO + '%ce%bf%ce%b9%ce%ba%ce%bf%ce%b3%ce%ad%ce%bd%ce%b5%ce%b9%ce%b1/', 4, ART + 'top.png', FANART, '')
     addDir('[B][COLOR gold]' + Lang(32002) + '[/COLOR][/B]', GAMATO, 18, ICON, FANART, '')
     views.selectView('menu', 'menu-view')
 
