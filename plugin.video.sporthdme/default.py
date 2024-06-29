4a5d2356a1528cf28db5cf95e758f8faddd72d6e
bugatsinho <bugatsinho@protonmail.com>
2024-06-29 10:18:34 -0500

    update SportHD to 0.1.58
    fix matches regex credits to GKOBU

--- plugin.video.sporthdme/default.py
+++ plugin.video.sporthdme/default.py
@@ -71,7 +71,7 @@ def get_events(url):  # 5
 
     # matches = re.findall('''null\,(\{"(?:matches|customNotFoundMessage).+?)\]\}\]n''', events, re.DOTALL)[0]
     # pattern = r'("matches"\s*\:\s*\[.+?])}]}]n"'
-    pattern = r'"matches"\s*\:\s*(\[.+?])}]}]n'
+    pattern = r'"matches"\s*\:\s*(\[.+?])\s*,\s*"websiteConfig"' #r'"matches"\s*\:\s*(\[.+?])}]}]n'
     matches = re.findall(pattern, events, re.DOTALL)[0]
     # xbmc.log('EVENTSSS: {}'.format(matches))
     matches = json.loads(matches)
