2087e648e228423c166eb1ea0481271cef4cdf29
Bugatsinho <bugatsinho@protonmail.com>
2026-08-22 12:48:22 -0500

    Revert "Update plugin.video.rlsbb to 1.0.0"
    This reverts commit 9171953255454ccccd7789e00634fc4ebde21a46.

--- plugin.video.rlsbb/resources/lib/modules/init.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-from sys import argv
-from urllib.parse import parse_qsl
-
-# service.py runs standalone at Kodi startup (not invoked as plugin://...), so it
-# only gets argv[0] -- no handle/query. Default those instead of crashing on import.
-sysaddon = argv[0]
-syshandle = int(argv[1]) if len(argv) > 1 else -1
-params = dict(parse_qsl(argv[2].replace('?', ''))) if len(argv) > 2 else {}
